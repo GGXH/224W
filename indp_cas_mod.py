da8d6b58cb8fb119e3f9105279839d5b38c32e2a
GGXH <guoxin_94@hotmail.com>
2016-11-05 00:12:11 -0700

    update independent cascade model and linear threshold model with formal random seed initialization

--- indp_cas_mod.py
+++ indp_cas_mod.py
@@ -29,53 +29,41 @@ def get_graph_info(gf_file):
                     print "wrong format line: ", line
     return gf_map
 
-def lnr_thres_mod_1step_v2(gf_map, act_nod_list, new_act_nod, nd_prob_map, nbr_map):
+def ind_cas_mod_1step_v2(gf_map, act_nod_list, new_act_nod, prob):
     '''
-    perform 1 step linear threshold model model
+    perform 1 step independent cascade model
     '''
     new_pre_nod = set()
     for id in new_act_nod:
         for nbr_id in gf_map[id]:
             if nbr_id not in act_nod_list:
-                if nbr_id not in nbr_map:
-                    count = 0
-                    for nbr_nbr_id in gf_map[nbr_id]:
-                        if nbr_nbr_id in act_nod_list:
-                            count += 1
-                    nbr_map[nbr_id] = count
-                else:
-                    nbr_map[nbr_id] += 1
-                if nbr_id not in nd_prob_map:
-                    nd_prob_map[nbr_id] = random.randint(1, len(gf_map[nbr_id]))
-                if nbr_map[nbr_id] >= nd_prob_map[nbr_id]:
+                lim = random.random()
+                if lim < prob:
                     new_pre_nod.add(nbr_id)
                     act_nod_list.add(nbr_id)
-                    nd_prob_map.pop(nbr_id)
-                    nbr_map.pop(nbr_id)
     return act_nod_list, new_pre_nod
-
 
 if __name__ == "__main__":
     gf_file = "data/com-lj.ungraph.txt"
+    gf_map = get_graph_info(gf_file)
+    gf_file = "data/com-lj.ungraph.txt"
     gf = snap.LoadEdgeList(snap.PUNGraph, gf_file, 0, 1)
     print "Load graph! With nodes ", gf.GetNodes(), " and edges ", gf.GetEdges()
-    gf_map = get_graph_info(gf_file)
     ##--get run the model with random initial
+    prob = 0.01
     mean_list = {}
     std_list = {}
     total_iter = 1000000
-    for init_set in xrange(30):
+    for init_set in xrange(1, 31):
         total_influence = [0] * total_iter
         for i in xrange(total_iter):
             new_act_nod = set()
-            nd_prob_map = {}
-            nbr_map = {}
             for j in xrange(init_set):
                 new_act_nod.add(gf.GetRndNId())
             act_nod_list = new_act_nod.copy()
             step = 0
-            while len(new_act_nod) != 0 and step < 5:
-                act_nod_list, new_act_nod = lnr_thres_mod_1step_v2(gf_map, act_nod_list, new_act_nod, nd_prob_map, nbr_map)
+            while len(new_act_nod) != 0 and step < 10:
+                act_nod_list, new_act_nod = ind_cas_mod_1step_v2(gf_map, act_nod_list, new_act_nod, prob)
                 step += 1
             if i % (total_iter / 10) == 0:
                 print init_set, i, len(act_nod_list), len(new_act_nod)
@@ -84,7 +72,8 @@ if __name__ == "__main__":
         mean_list[init_set] = total_influence.mean()[0]
         std_list[init_set] = total_influence.std()[0]
         print init_set, mean_list[init_set], std_list[init_set]
-    ##--dump file
-    with open('lnr_thrs_rnd.txt', 'wb') as fl:
+    ##--dump to file
+    with open('indp_cas_rnd_10.pkl', 'wb') as fl:
         pickle.dump(mean_list, fl)
         pickle.dump(std_list, fl)
+    
